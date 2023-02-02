da5668dc560fffc0adf62aa1ad0fc36f25591d73
Le-Andris Daniel <ldaniel38@gmail.com>
2023-02-01 22:24:51 -0600

    update model definition

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,20 +1,15 @@
 import mongoose from 'mongoose';
+import { IngredientSchema } from './ingredientModel.js';
+import utils from '../utils.js';
+
 const { Schema } = mongoose;
+const { getDecimal, setDecimal } = utils;
 
-const getDecimal = (value) =>
-  typeof value !== 'undefined' ? parseFloat(value.toString()) : value;
+IngredientSchema.add({
+  amount: { type: mongoose.Types.Decimal128, get: getDecimal, set: setDecimal }
+});
 
-const setDecimal = (value) => {
-  if (typeof value !== 'undefined') {
-    let convertedValue = parseFloat(value.toString());
-
-    return isNaN(convertedValue) ? value : convertedValue;
-  } else {
-    return value;
-  }
-};
-
-const OperationSchema = new Schema(
+const RecipeSchema = new Schema(
   {
     order: {
       type: Number,
@@ -24,12 +19,6 @@ const OperationSchema = new Schema(
       type: String,
       required: [true, 'Please enter an operation step description']
     },
-    instructions: [
-      {
-        order: Number,
-        instruction: String
-      }
-    ],
     target: {
       required: [true, 'Please enter a target limit'],
       type: mongoose.Types.Decimal128,
@@ -40,34 +29,22 @@ const OperationSchema = new Schema(
       type: String,
       required: [true, 'Please enter a target unit of measure']
     },
-    ingredients: [
+    ingredients: [IngredientSchema],
+    instructions: [
       {
-        ingredient: {
-          type: mongoose.Types.ObjectId,
-          ref: 'Ingredients'
+        order: {
+          type: Number,
+          required: [true, 'Please enter an instruction order number']
         },
-        amount: {
-          type: mongoose.Types.Decimal128,
-          get: getDecimal,
-          set: setDecimal
+        instruction: {
+          type: String,
+          required: [true, 'Please enter an instruction detail']
         }
       }
     ]
   },
   { toJSON: { getters: true } }
 );
-
-/*
-TODO: https://stackoverflow.com/questions/45475639/mongoose-set-this-field-to-true-others-will-be-false
-RecipeSchema.pre('save', function (next) {
-  if (this.active === true) {
-    this.constructor
-      .update({}, { $set: { default: false } }, { multi: true })
-      .exec();
-  }
-  next();
-});
-*/
 
 const ProductSchema = new Schema(
   {
@@ -86,9 +63,7 @@ const ProductSchema = new Schema(
       required: [true, 'Please enter a product category']
     },
     image: String,
-    recipe: {
-      operations: [OperationSchema]
-    },
+    recipe: [RecipeSchema],
     tags: [{ tag: String }]
   },
   { timestamps: true, toJSON: { getters: true } }
