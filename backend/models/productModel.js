48df2be85e8ae78cffde65fc4956bb2498dff662
Le-Andris Daniel <ldaniel38@gmail.com>
2023-02-13 18:57:50 -0600

    change model type to fix $numberDecimal error

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,56 +4,55 @@ import utils from '../utils.js';
 const { Schema } = mongoose;
 const { getDecimal, setDecimal } = utils;
 
-const RecipeSchema = new Schema(
-  {
-    order: {
-      type: Number,
-      required: [true, 'Please enter an step order number']
-    },
-    description: {
-      type: String,
-      required: [true, 'Please enter an operation step description']
-    },
-    ingredients: [
-      {
-        name: {
-          type: String,
-          required: [true, 'Please add an ingredient name value']
-        },
-        uom: {
-          type: String,
-          required: [true, 'Please add an ingredient unit of measurement value']
-        },
-        uomAbbreviation: {
-          type: String,
-          required: [
-            true,
-            'Please add an ingredient unit of measurement abbreviation value'
-          ]
-        },
-        amount: {
-          required: [true, 'Please enter an ingredient amount'],
-          type: mongoose.Types.Decimal128,
-          get: getDecimal,
-          set: setDecimal
-        }
-      }
-    ],
-    instructions: [
-      {
-        order: {
-          type: Number,
-          required: [true, 'Please enter an instruction order number']
-        },
-        instruction: {
-          type: String,
-          required: [true, 'Please enter an instruction detail']
-        }
-      }
-    ]
+const RecipeSchema = new Schema({
+  order: {
+    type: Number,
+    required: [true, 'Please enter an step order number']
   },
-  { toJSON: { getters: true } }
-);
+  description: {
+    type: String,
+    required: [true, 'Please enter an operation step description']
+  },
+  ingredients: [
+    {
+      name: {
+        type: String,
+        required: [true, 'Please add an ingredient name value']
+      },
+      uom: {
+        type: String,
+        required: [true, 'Please add an ingredient unit of measurement value']
+      },
+      uomAbbreviation: {
+        type: String,
+        required: [
+          true,
+          'Please add an ingredient unit of measurement abbreviation value'
+        ]
+      },
+      amount: {
+        required: [true, 'Please enter an ingredient amount'],
+        default: 0,
+        min: 0,
+        type: Number,
+        get: getDecimal,
+        set: setDecimal
+      }
+    }
+  ],
+  instructions: [
+    {
+      order: {
+        type: Number,
+        required: [true, 'Please enter an instruction order number']
+      },
+      instruction: {
+        type: String,
+        required: [true, 'Please enter an instruction detail']
+      }
+    }
+  ]
+});
 
 const ProductSchema = new Schema(
   {
